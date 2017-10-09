54e9aa16079ddad81b713c38f2d77c7ce2c9c699
lookpud <32553473+lookpud@users.noreply.github.com>
2017-10-09 11:22:13 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -28,7 +28,7 @@ if (!is_null($events['events'])) {
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => .$events['events'][0]['source']['userId'],
+				'messages' => [$messages],// .$events['events'][0]['source']['userId']
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
