2f0e0febfe9e8bf19daadf94769b208c646f032a
lookpud <32553473+lookpud@users.noreply.github.com>
2017-10-09 11:39:45 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -21,14 +21,14 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $event['source']['userId']//$text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => $event['source']['userId'],// [$messages].
+				'messages' => [$messages],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
@@ -46,6 +46,6 @@ if (!is_null($events['events'])) {
 		}
 	}
 }
-echo "OK1";
+echo "OK2";
 	
 
