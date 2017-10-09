51113f3977b81a8693ff29f06c26e3afeace4edf
lookpud <32553473+lookpud@users.noreply.github.com>
2017-10-09 19:46:01 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -11,8 +11,8 @@ $access_token = 'mp9W1fQUWXhFHXoIzL7fGy0sW55YeJX3w+2/q/L7zeQa4Ouk/xK1aUypnqo0lFg
 //
 ini_set("allow_url_fopen", 1);
 $json = file_get_contents('http://139.59.247.234:1337/myApi/2');
-$obj = json_decode($json, true);
-echo $obj . "\r\n";
+// $obj = json_decode($json, true);
+echo $json . "\r\n";
 
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
