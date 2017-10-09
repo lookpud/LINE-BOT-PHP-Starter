9574d8746ef332b6c1980286fa8846cf61169dae
lookpud <32553473+lookpud@users.noreply.github.com>
2017-10-09 11:11:54 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -50,7 +50,7 @@ echo "OK";
 	
 // curl -X POST \
 // -H 'Content-Type:application/json' \
-// -H 'Authorization: Bearer mp9W1fQUWXhFHXoIzL7fGy0sW55YeJX3w+2/q/L7zeQa4Ouk/xK1aUypnqo0lFg9hN5GyFN/v/HmDARGeep1o9Pm8kEzQ/h6JA8kxwFAxXUvmF7cEaPm9u6/pMdFWay5FEc35vYlxceDLvixuLzmSwdB04t89/1O/w1cDnyilFU=' \
+// -H 'Authorization: Bearer {mp9W1fQUWXhFHXoIzL7fGy0sW55YeJX3w+2/q/L7zeQa4Ouk/xK1aUypnqo0lFg9hN5GyFN/v/HmDARGeep1o9Pm8kEzQ/h6JA8kxwFAxXUvmF7cEaPm9u6/pMdFWay5FEc35vYlxceDLvixuLzmSwdB04t89/1O/w1cDnyilFU=}' \
 // -d '{
 //     "to": ["xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"],
 //     "messages":[
