ba4b3d55ed4a395bf1bc87c27137af602ae7e177
lookpud <32553473+lookpud@users.noreply.github.com>
2017-10-11 15:08:32 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -88,10 +88,10 @@ function msgPush(){
 	ini_set("allow_url_fopen", 1);
 	$json = file_get_contents('http://139.59.247.234:1337/myApi/17');
 	$obj = json_decode($json, true);
-	echo $obj . "\n";
-	foreach($obj as $key => $value){
-  		echo 'value' . $value . '<br />';
-	}
+	echo $obj->Title . "\n";
+// 	foreach($json as $key => $value){
+//   		echo 'value' . $value . '<br />';
+// 	}
 	
 	// Build message to reply back
 	$sticker = [
