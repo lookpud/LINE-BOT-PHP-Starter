7d5e9d9a6a94bc17d9bef106641ddbfde0c9a741
lookpud <32553473+lookpud@users.noreply.github.com>
2017-10-11 10:55:32 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,5 +1,9 @@
 <?php
 
+msgPush();
+
+
+function msgReply(){
 $access_token = 'mp9W1fQUWXhFHXoIzL7fGy0sW55YeJX3w+2/q/L7zeQa4Ouk/xK1aUypnqo0lFg9hN5GyFN/v/HmDARGeep1o9Pm8kEzQ/h6JA8kxwFAxXUvmF7cEaPm9u6/pMdFWay5FEc35vYlxceDLvixuLzmSwdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
@@ -8,33 +12,14 @@ $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
 
-//
-ini_set("allow_url_fopen", 1);
-$json = file_get_contents('http://139.59.247.234:1337/myApi/17');
-$obj = json_decode($json, true);
-echo [$json] . "\n";
-
 // try
+// foreach($messages as $key => $value){
+//   	echo $key . '<br />';
+// 	foreach($value as $key_arr => $value_arr){
+//   		echo $key_arr . ': ' . implode('-', $values_arr) . '<br /><br />';
+// 	}
+// }
 
-			$sticker = [
-				'type' => 'sticker',
-				'packageId' => '1',
-    				'stickerId' => '1'
-			];
-			$reply = [
-				'type' => 'text',
-				'text' => 'zzZZ'
-			];
-			$messages = [
-					$sticker,
-					 $reply
-				];
-			foreach($messages as $key => $value){
-  				echo $key . '<br />';
-				foreach($value as $key_arr => $value_arr){
-  					echo $key_arr . ': ' . implode('-', $values_arr) . '<br /><br />';
-				}
-			}
 // Validate parsed JSON data
 if (!is_null($events['events'])) {
 	// Loop through each event
@@ -68,10 +53,7 @@ if (!is_null($events['events'])) {
 					'text' => 'Say "Hi" to me' 
 				];
 			}
-				
-				
 			
-
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
@@ -96,4 +78,50 @@ if (!is_null($events['events'])) {
 	}
 }
 echo "OK2";
+}
+
+
+function msgPush(){
+	$access_token = 'mp9W1fQUWXhFHXoIzL7fGy0sW55YeJX3w+2/q/L7zeQa4Ouk/xK1aUypnqo0lFg9hN5GyFN/v/HmDARGeep1o9Pm8kEzQ/h6JA8kxwFAxXUvmF7cEaPm9u6/pMdFWay5FEc35vYlxceDLvixuLzmSwdB04t89/1O/w1cDnyilFU=';
+	
+	//
+	ini_set("allow_url_fopen", 1);
+	$json = file_get_contents('http://139.59.247.234:1337/myApi/17');
+	$obj = json_decode($json, true);
+	echo [$json] . "\n";
+	
+	// Build message to reply back
+	$messages = [
+		'sticker' => $sticker,
+		'reply' => $reply
+	];
+			
+	// Make a POST Request to Messaging API to reply to sender
+	$url = 'https://api.line.me/v2/bot/message/reply';
+	$data = [
+		'to' => 'Ua7085916d72ba072759cfa5fe05ac3b8',
+		'messages' => [$messages],
+	];
+	echo data . $data . "\n";
+	$post = json_encode($data);
+	$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+	$ch = curl_init($url);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	$result = curl_exec($ch);
+	curl_close($ch);
+
+	echo result . $result . "\r\n";
+	echo "OK1";
+}
+
+
+
+
+
+
 
