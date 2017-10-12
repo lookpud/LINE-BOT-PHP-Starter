a5b5d986665f20da515ee80af505c5471f09daab
lookpud <32553473+lookpud@users.noreply.github.com>
2017-10-12 15:52:08 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -143,6 +143,24 @@ function msgPush(){
 }
 
 
+curl -X POST \
+-H 'Content-Type:application/json' \
+-H 'Authorization: Bearer {mp9W1fQUWXhFHXoIzL7fGy0sW55YeJX3w+2/q/L7zeQa4Ouk/xK1aUypnqo0lFg9hN5GyFN/v/HmDARGeep1o9Pm8kEzQ/h6JA8kxwFAxXUvmF7cEaPm9u6/pMdFWay5FEc35vYlxceDLvixuLzmSwdB04t89/1O/w1cDnyilFU=}' \
+-d '{
+    "to": "Ua7085916d72ba072759cfa5fe05ac3b8",
+    "messages":[
+        {
+            "type":"text",
+            "text":"Hello, world1"
+        },
+        {
+            "type":"text",
+            "text":"Hello, world2"
+        }
+    ]
+}' https://api.line.me/v2/bot/message/push
+
+
 
 
 
