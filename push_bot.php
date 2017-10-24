babc31e18f725a82103dd7b6d7ae07f539cea08b
lookpud <32553473+lookpud@users.noreply.github.com>
2017-10-24 15:03:45 +0700

    Update push_bot.php

--- push_bot.php
+++ push_bot.php
@@ -7,7 +7,7 @@ function msgPush(){
 	ini_set("allow_url_fopen", 1);
 	$json = file_get_contents('http://139.59.247.234:1337/TestBot/FindUser/findUser');
 	$obj = json_decode($json);
-	echo $obj[0]->LUID[0]->LUID . "\n";
+	echo $obj[0] . "\n";
 // 	foreach($json as $key => $value){
 //   		echo 'value' . $value . '<br />';
 // 	}
@@ -43,22 +43,22 @@ function msgPush(){
 	print 'msg: ' . [$messages] . "\n";
 			
 	// Make a POST Request to Messaging API to reply to sender
-	$url = 'https://api.line.me/v2/bot/message/push';
-	$data = [
-		'to' => '',
-		'messages' => $arr,
-	];
-	echo 'data: ' . $data . "\n";
-	$post = json_encode($data);
-	$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-	$ch = curl_init($url);
-	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-	$result = curl_exec($ch);
-	curl_close($ch);
-	echo 'result: ' . $result . "\r\n";
-	echo "OK2";
+// 	$url = 'https://api.line.me/v2/bot/message/push';
+// 	$data = [
+// 		'to' => '',
+// 		'messages' => $arr,
+// 	];
+// 	echo 'data: ' . $data . "\n";
+// 	$post = json_encode($data);
+// 	$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+// 	$ch = curl_init($url);
+// 	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+// 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+// 	curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+// 	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+// 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+// 	$result = curl_exec($ch);
+// 	curl_close($ch);
+// 	echo 'result: ' . $result . "\r\n";
+// 	echo "OK2";
 }
